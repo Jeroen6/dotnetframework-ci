991ab93ac33c73a5c2170c17e8db5e314b5bbd34
Jeroen Lodder <j.lodder@emri.nl>
2023-07-20 08:01:28 +0200

    Initial project

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace dotnetframework_ci
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
